c07d65e3336393794d04a3d0d6845a5d4779899e
Ludwik1526 <mmichal1526@gmail.com>
2020-10-20 14:09:55 +0200

    Leaving the game

---
+++ ARcade Madness/Assets/LukasJ/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject pauseScreen, optWindow, escWindow;
+
+    public Button optButton, escButton, escCancelButton, resumeButton;
+    
+
+    void Start()
+    {
+        pauseScreen.SetActive(false);
+        optWindow.SetActive(false);
+        escWindow.SetActive(false);
+
+        optButton.onClick.AddListener(OpenPauseScreen);
+        escButton.onClick.AddListener(OpenEscWindow);
+        escCancelButton.onClick.AddListener(CloseEscWindow);
+        resumeButton.onClick.AddListener(ClosePauseScreen);
+    }
+
+    void OpenPauseScreen()
+    {
+        pauseScreen.SetActive(true);
+        OpenOptWindow();
+    }
+
+    void ClosePauseScreen()
+    {
+        pauseScreen.SetActive(false);
+    }
+
+    void OpenOptWindow()
+    {
+        optWindow.SetActive(true);
+    }
+
+    void CloseOptWindow()
+    {
+        optWindow.SetActive(false);
+    }
+
+    void OpenEscWindow()
+    {
+        escWindow.SetActive(true);
+        CloseOptWindow();
+    }
+
+    void CloseEscWindow()
+    {
+        escWindow.SetActive(false);
+        OpenOptWindow();
+    }
+}
--- ARcade Madness/Assets/Michal/Scripts/GameSetupController.cs
+++ ARcade Madness/Assets/Michal/Scripts/GameSetupController.cs
@@ -1,9 +1,14 @@
 ﻿using Photon.Pun;
+using System.Collections;
 using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameSetupController : MonoBehaviour
 {
+    [SerializeField]
+    private int menuSceneIndex;
+
     private void Start()
     {
         CreatePlayer();
@@ -12,5 +17,19 @@ public class GameSetupController : MonoBehaviour
     private void CreatePlayer()
     {
         PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Player"), Vector3.zero, Quaternion.identity);
+    }
+
+    public void DisconnectPlayer()
+    {
+        StartCoroutine("DisconnectAndLoad");
+    }
+
+    IEnumerator DisconnectAndLoad()
+    {
+        yield return new WaitForSeconds(1f);
+        PhotonNetwork.Disconnect();
+        while (PhotonNetwork.IsConnected)
+            yield return null;
+        SceneManager.LoadScene(menuSceneIndex);
     }
 }
