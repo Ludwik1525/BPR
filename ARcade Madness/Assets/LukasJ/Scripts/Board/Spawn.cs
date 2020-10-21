3811018130634486edbc6fb916d1e0c1daa0e6c7
BestProjectMember <38316729+BestProjectMember@users.noreply.github.com>
2020-10-21 11:44:30 +0200

    Added spawning

---
+++ ARcade Madness/Assets/LukasJ/Scripts/Board/Spawn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    private SpawnPosition[] spawnPositions;
+
+    private void Awake()
+    {
+        spawnPositions = GetComponentsInChildren<SpawnPosition>();
+    }
+
+    public Transform AssignSpawnPosition()
+    {
+       foreach(SpawnPosition pos in spawnPositions)
+       {
+            if(pos.isFree)
+            {
+                pos.isFree = false;
+                return pos.transform;
+            }
+       }
+        return null;
+    }
+
+    public void LeaveSpawnPosition(SpawnPosition position)
+    {
+        foreach (SpawnPosition pos in spawnPositions)
+        {
+            if(pos == position)
+            {
+                pos.isFree = true;
+            }
+        }
+    }
+
+
+
+}
---
+++ ARcade Madness/Assets/LukasJ/Scripts/Board/SpawnPosition.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPosition : MonoBehaviour
+{
+    [HideInInspector]
+    public bool isFree = true;
+}
