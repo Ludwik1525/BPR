d979110a9e9b205f58a3f502a21702535b20a2d7
Ludwik1526 <mmichal1526@gmail.com>
2020-10-21 15:03:56 +0200

    Merge conflict resolved

--- ARcade Madness/Assets/LukasJ/Scripts/Board/PlayerColour.cs
+++ ARcade Madness/Assets/LukasJ/Scripts/Board/PlayerColour.cs
@@ -16,10 +16,7 @@ public class PlayerColour : MonoBehaviour
     {
         PV = GetComponent<PhotonView>();
         colours = FindObjectOfType<ColourPalette>();
-<<<<<<< HEAD
 
-=======
->>>>>>> e50726512fdb27018e0199c7ab662c4aa2ad9060
         for (int i = GameSetupController.players.Count - 1; i >= 0; i--)
         {
             GameSetupController.players[i].transform.GetChild(1).GetChild(0).GetChild(0).GetComponent<SkinnedMeshRenderer>().material =
