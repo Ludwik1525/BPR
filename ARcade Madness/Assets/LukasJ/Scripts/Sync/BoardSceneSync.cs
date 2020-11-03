b589a4c0afed3ee44d0abdacdbabcc15ef287e00
BestProjectMember <38316729+BestProjectMember@users.noreply.github.com>
2020-11-03 14:36:35 +0100

    Added Sync script for players lag

---
+++ ARcade Madness/Assets/LukasJ/Scripts/Sync/BoardSceneSync.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class BoardSceneSync : MonoBehaviour, IPunObservable
+{
+    PhotonView photonView;
+
+    Vector3 networkedPosition;
+    Quaternion networkedRotation;
+
+    public bool synchronizeVelocity = true;
+    public bool synchronizeAngularVelocity = true;
+    public bool isTeleportEnabled = true;
+    public float teleportIfDistanceGreaterThan = 1.0f;
+
+    private float distance;
+    private float angle;
+
+    private void Awake()
+    {
+        photonView = GetComponent<PhotonView>();
+
+        networkedPosition = new Vector3();
+        networkedRotation = new Quaternion();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!photonView.IsMine)
+        {
+            // update "RemoteME"
+            transform.position = Vector3.MoveTowards(transform.position, networkedPosition, distance * (1.0f / PhotonNetwork.SerializationRate));
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, networkedRotation, angle * (1.0f / PhotonNetwork.SerializationRate));
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            //Then, photonView is mine and I am the one who controls the player
+            //should send postion, velocity etc. data to the other players 
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            //Called on my player gameobject that exists in remote player's game
+            networkedPosition = (Vector3)stream.ReceiveNext();
+            networkedRotation = (Quaternion)stream.ReceiveNext();
+
+            if (isTeleportEnabled)
+            {
+                if (Vector3.Distance(transform.position, networkedPosition) > teleportIfDistanceGreaterThan)
+                {
+                    transform.position = networkedPosition;
+                }
+            }
+        }
+    }
+}
