d7e8255f4113448dae8867b24074659cca439ec1
BestProjectMember <38316729+BestProjectMember@users.noreply.github.com>
2020-11-03 14:39:57 +0100

    Sync script updated

--- ARcade Madness/Assets/LukasJ/Scripts/Sync/BoardSceneSync.cs
+++ ARcade Madness/Assets/LukasJ/Scripts/Sync/BoardSceneSync.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class BoardSceneSync : MonoBehaviour, IPunObservable
+public class BoardSeneSync: MonoBehaviour, IPunObservable
 {
+    Rigidbody rb;
     PhotonView photonView;
 
     Vector3 networkedPosition;
@@ -18,12 +19,17 @@ public class BoardSceneSync : MonoBehaviour, IPunObservable
     private float distance;
     private float angle;
 
+    private GameObject battleArenaGameobject;
+
     private void Awake()
     {
+        rb = GetComponent<Rigidbody>();
         photonView = GetComponent<PhotonView>();
 
         networkedPosition = new Vector3();
         networkedRotation = new Quaternion();
+
+        battleArenaGameobject = GameObject.Find("BattleArena");
     }
 
     private void FixedUpdate()
@@ -31,8 +37,8 @@ public class BoardSceneSync : MonoBehaviour, IPunObservable
         if (!photonView.IsMine)
         {
             // update "RemoteME"
-            transform.position = Vector3.MoveTowards(transform.position, networkedPosition, distance * (1.0f / PhotonNetwork.SerializationRate));
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, networkedRotation, angle * (1.0f / PhotonNetwork.SerializationRate));
+            rb.position = Vector3.MoveTowards(rb.position, networkedPosition, distance * (1.0f / PhotonNetwork.SerializationRate));
+            rb.rotation = Quaternion.RotateTowards(rb.rotation, networkedRotation, angle * (1.0f / PhotonNetwork.SerializationRate));
         }
     }
 
@@ -42,20 +48,53 @@ public class BoardSceneSync : MonoBehaviour, IPunObservable
         {
             //Then, photonView is mine and I am the one who controls the player
             //should send postion, velocity etc. data to the other players 
-            stream.SendNext(transform.position);
-            stream.SendNext(transform.rotation);
+            stream.SendNext(rb.position - battleArenaGameobject.transform.position);
+            stream.SendNext(rb.rotation);
+
+            if (synchronizeVelocity)
+            {
+                stream.SendNext(rb.velocity);
+            }
+
+            if (synchronizeAngularVelocity)
+            {
+                stream.SendNext(rb.angularVelocity);
+            }
         }
         else
         {
             //Called on my player gameobject that exists in remote player's game
-            networkedPosition = (Vector3)stream.ReceiveNext();
+            networkedPosition = (Vector3)stream.ReceiveNext() + battleArenaGameobject.transform.position;
             networkedRotation = (Quaternion)stream.ReceiveNext();
 
             if (isTeleportEnabled)
             {
-                if (Vector3.Distance(transform.position, networkedPosition) > teleportIfDistanceGreaterThan)
+                if (Vector3.Distance(rb.position, networkedPosition) > teleportIfDistanceGreaterThan)
                 {
-                    transform.position = networkedPosition;
+                    rb.position = networkedPosition;
+                }
+            }
+
+            if (synchronizeVelocity || synchronizeAngularVelocity)
+            {
+                float lag = Mathf.Abs((float)(PhotonNetwork.Time - info.SentServerTime));
+
+                if (synchronizeVelocity)
+                {
+                    rb.velocity = (Vector3)stream.ReceiveNext();
+
+                    networkedPosition += rb.velocity * lag;
+
+                    distance = Vector3.Distance(rb.position, networkedPosition);
+                }
+
+                if (synchronizeAngularVelocity)
+                {
+                    rb.angularVelocity = (Vector3)stream.ReceiveNext();
+
+                    networkedRotation = Quaternion.Euler(rb.angularVelocity * lag) * networkedRotation;
+
+                    angle = Quaternion.Angle(rb.rotation, networkedRotation);
                 }
             }
         }
