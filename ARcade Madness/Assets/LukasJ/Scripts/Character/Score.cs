ca75fe2a63d35a23a1cadcc449a430a8a081fd07
BestProjectMember <38316729+BestProjectMember@users.noreply.github.com>
2020-11-10 14:53:12 +0100

    scoreee

--- ARcade Madness/Assets/LukasJ/Scripts/Character/Score.cs
+++ ARcade Madness/Assets/LukasJ/Scripts/Character/Score.cs
@@ -9,6 +9,7 @@ public class Score : MonoBehaviour
     private ScoreInfo si;
     private PhotonView myPV;
     private int score;
+    ExitGames.Client.Photon.Hashtable thisScore;
 
     private void Start()
     {
@@ -16,12 +17,13 @@ public class Score : MonoBehaviour
         myPV = GetComponent<PhotonView>();
         if(myPV.IsMine)
         {
-            if(PhotonNetwork.LocalPlayer.CustomProperties["score"] != null)
+            thisScore = new ExitGames.Client.Photon.Hashtable();
+            if (PhotonNetwork.LocalPlayer.CustomProperties["score"] != null)
                 si.GetComponent<PhotonView>().RPC("SetScore", RpcTarget.AllBuffered, (int)PhotonNetwork.LocalPlayer.CustomProperties["PlayerNo"],
                     (int)PhotonNetwork.LocalPlayer.CustomProperties["score"]);
             else
             {
-                ExitGames.Client.Photon.Hashtable thisScore = new ExitGames.Client.Photon.Hashtable();
+                print("ELSE");
                 thisScore.Add("score", 0);
                 PhotonNetwork.LocalPlayer.SetCustomProperties(thisScore);
             }
@@ -31,23 +33,19 @@ public class Score : MonoBehaviour
 
     public void setScore(int score)
     {
-        this.score += score;
 
-        ExitGames.Client.Photon.Hashtable thisScore = new ExitGames.Client.Photon.Hashtable();
+        print("Adding 1 point");
+        this.score += score;
         thisScore.Add("score", score);
         PhotonNetwork.LocalPlayer.SetCustomProperties(thisScore);
 
         if(myPV.IsMine)
             si.GetComponent<PhotonView>().RPC("SetScore", RpcTarget.AllBuffered, (int)PhotonNetwork.LocalPlayer.CustomProperties["PlayerNo"], this.score);
     }
-    public int getScore()
-    {
-        return PlayerPrefs.GetInt("score");
-    }
 
     private void OnApplicationQuit()
     {
-        PlayerPrefs.SetInt("score", 0);
+        //PlayerPrefs.SetInt("score", 0);
     }
 
 }
