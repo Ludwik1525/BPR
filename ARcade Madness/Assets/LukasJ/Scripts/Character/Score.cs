466bd331a051345881aee99223b368280e4e7664
Ludwik1526 <mmichal1526@gmail.com>
2020-11-10 15:02:33 +0100

    Score v1.7

--- ARcade Madness/Assets/LukasJ/Scripts/Character/Score.cs
+++ ARcade Madness/Assets/LukasJ/Scripts/Character/Score.cs
@@ -9,43 +9,42 @@ public class Score : MonoBehaviour
     private ScoreInfo si;
     private PhotonView myPV;
     private int score;
-    ExitGames.Client.Photon.Hashtable thisScore;
 
     private void Start()
     {
         si = FindObjectOfType<ScoreInfo>();
         myPV = GetComponent<PhotonView>();
+
+        if(PlayerPrefs.HasKey("Score"))
+        {
+            score = PlayerPrefs.GetInt("Score");
+        }
+        else
+        {
+            score = 0;
+        }
+
         if(myPV.IsMine)
         {
-            thisScore = new ExitGames.Client.Photon.Hashtable();
-            if (PhotonNetwork.LocalPlayer.CustomProperties["score"] != null)
-                si.GetComponent<PhotonView>().RPC("SetScore", RpcTarget.AllBuffered, (int)PhotonNetwork.LocalPlayer.CustomProperties["PlayerNo"],
-                    (int)PhotonNetwork.LocalPlayer.CustomProperties["score"]);
-            else
-            {
-                print("ELSE");
-                thisScore.Add("score", 0);
-                PhotonNetwork.LocalPlayer.SetCustomProperties(thisScore);
-            }
+            si.GetComponent<PhotonView>().RPC("SetScore", RpcTarget.AllBuffered, (int)PhotonNetwork.LocalPlayer.CustomProperties["PlayerNo"],
+                       PlayerPrefs.GetInt("Score"));
         }
-        
     }
 
     public void setScore(int score)
     {
-
-        print("Adding 1 point");
         this.score += score;
-        thisScore.Add("score", score);
-        PhotonNetwork.LocalPlayer.SetCustomProperties(thisScore);
 
-        if(myPV.IsMine)
-            si.GetComponent<PhotonView>().RPC("SetScore", RpcTarget.AllBuffered, (int)PhotonNetwork.LocalPlayer.CustomProperties["PlayerNo"], this.score);
+        PlayerPrefs.SetInt("Score", score);
+
+        if (myPV.IsMine)
+        {
+            si.GetComponent<PhotonView>().RPC("SetScore", RpcTarget.AllBuffered, (int)PhotonNetwork.LocalPlayer.CustomProperties["PlayerNo"], PlayerPrefs.GetInt("Score"));
+        }
     }
 
     private void OnApplicationQuit()
     {
-        //PlayerPrefs.SetInt("score", 0);
+        PlayerPrefs.SetInt("Score", 0);
     }
-
 }
