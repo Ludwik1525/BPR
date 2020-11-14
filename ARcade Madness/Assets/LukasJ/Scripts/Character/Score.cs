d7a62dbe6f77771af4e7d37156faaee9a6f5c01d
BestProjectMember <38316729+BestProjectMember@users.noreply.github.com>
2020-11-14 14:49:19 +0100

    Revert "Fix chest v2"
    This reverts commit b4f66a14eb99b957efdb87c76c275dc5c444d30d.

--- ARcade Madness/Assets/LukasJ/Scripts/Character/Score.cs
+++ ARcade Madness/Assets/LukasJ/Scripts/Character/Score.cs
@@ -28,6 +28,7 @@ public class Score : MonoBehaviour
         {
             si.GetComponent<PhotonView>().RPC("SetScore", RpcTarget.AllBuffered, (int)PhotonNetwork.LocalPlayer.CustomProperties["PlayerNo"],
                        PlayerPrefs.GetInt("Score"));
+            print("My score " + PlayerPrefs.GetInt("Score"));
         }
     }
 
