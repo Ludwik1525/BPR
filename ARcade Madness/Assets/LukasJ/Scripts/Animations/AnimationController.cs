513b03fecd223c0f6e4cdca68eaaaf4dd407ce31
BestProjectMember <38316729+BestProjectMember@users.noreply.github.com>
2020-11-17 15:55:34 +0100

    rocket anim fix

--- ARcade Madness/Assets/LukasJ/Scripts/Animations/AnimationController.cs
+++ ARcade Madness/Assets/LukasJ/Scripts/Animations/AnimationController.cs
@@ -1,24 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class AnimationController : MonoBehaviour
 {
     private BoardPlayerController playerController;
     private Animator anim;
     private GameObject rocket;
+    private PhotonView PV;
 
     private void Start()
     {
+        PV = transform.parent.GetComponent<PhotonView>();
         rocket = gameObject.transform.GetChild(1).gameObject;
         anim = GetComponentInChildren<Animator>();
+
         playerController = transform.parent.GetComponent<BoardPlayerController>();
         playerController.onStartMoving.AddListener(delegate { StartBoolAnimationByName("isRunning"); });
         playerController.onStopMoving.AddListener(delegate { StopBoolAnimationByName("isRunning"); });
         playerController.onStartMovingWithRocket.AddListener(delegate { StartBoolAnimationByName("isRidingRocket"); });
-        playerController.onStartMovingWithRocket.AddListener(EnableRocket);
+        playerController.onStartMovingWithRocket.AddListener(delegate { PV.RPC("EnableRocket", RpcTarget.AllBuffered); });
         playerController.onStopMovingWithRocket.AddListener(delegate { StartBoolAnimationByName("isRidingRocket"); });
-        playerController.onStopMovingWithRocket.AddListener(DisableRocket);
+        playerController.onStopMovingWithRocket.AddListener(delegate { PV.RPC("DisableRocket", RpcTarget.AllBuffered); });
 
     }
 
@@ -32,11 +36,13 @@ public class AnimationController : MonoBehaviour
         anim.SetBool(animationName, false);
     }
 
+    [PunRPC]
     private void EnableRocket()
     {
         rocket.SetActive(true);
     }
 
+    [PunRPC]
     private void DisableRocket()
     {
         rocket.SetActive(false);
