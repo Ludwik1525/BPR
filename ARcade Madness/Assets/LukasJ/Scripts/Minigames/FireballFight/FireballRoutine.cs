343ab0ca5bf44bff90192afad0aa45285c0af50f
Ludwik1526 <mmichal1526@gmail.com>
2020-11-18 12:14:47 +0100

    FireballCast

---
+++ ARcade Madness/Assets/LukasJ/Scripts/Minigames/FireballFight/FireballRoutine.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballRoutine : MonoBehaviour
+{
+    public float speed = 0.01f;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        this.gameObject.transform.Translate(-Vector3.forward * Time.deltaTime * speed);
+    }
+}
