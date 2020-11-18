44c8e74323372437fbb6e89718fd88e2371f3874
Ludwik1526 <mmichal1526@gmail.com>
2020-11-18 17:47:35 +0100

    Explosions

---
+++ ARcade Madness/Assets/LukasJ/Scripts/Minigames/FireballFight/DestroyMe.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class DestroyMe : MonoBehaviour
+{
+    private PhotonView PV; 
+
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        StartCoroutine(DestroyMeAfterTime(3));
+    }
+    
+    IEnumerator DestroyMeAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        PV.RPC("KillMe", RpcTarget.AllBuffered);
+    }
+
+    [PunRPC]
+    void KillMe()
+    {
+        Destroy(this.gameObject);
+    }
+}
--- ARcade Madness/Assets/LukasJ/Scripts/Minigames/FireballFight/FireballRoutine.cs
+++ ARcade Madness/Assets/LukasJ/Scripts/Minigames/FireballFight/FireballRoutine.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using System.IO;
 
 public class FireballRoutine : MonoBehaviour
 {
     public float speed = 10f;
     private Rigidbody rb;
+    private PhotonView PV;
 
     private void Start()
     {
+        PV = GetComponent<PhotonView>();
         rb = GetComponent<Rigidbody>();
     }
 
@@ -22,8 +26,16 @@ public class FireballRoutine : MonoBehaviour
         if(other.tag == "Player")
         {
             other.GetComponentInChildren<Animator>().gameObject.GetComponent<FireBallAnimator>().Die();
+            PhotonNetwork.Instantiate(Path.Combine("SmallExplosion", "Player"), this.transform.position, Quaternion.identity);
         }
 
+        PhotonNetwork.Instantiate(Path.Combine("PlasmaExplosion", "Player"), this.transform.position, Quaternion.identity);
+        PV.RPC("KillMe", RpcTarget.AllBuffered);
+    }
+
+    [PunRPC]
+    void KillMe()
+    {
         Destroy(gameObject);
     }
 }
