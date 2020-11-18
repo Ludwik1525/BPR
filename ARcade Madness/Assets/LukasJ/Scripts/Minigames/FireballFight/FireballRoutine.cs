1d37bd6dce171a5ce1155cc2902ac88d54997a6c
BestProjectMember <38316729+BestProjectMember@users.noreply.github.com>
2020-11-18 14:35:25 +0100

    Shield added

--- ARcade Madness/Assets/LukasJ/Scripts/Minigames/FireballFight/FireballRoutine.cs
+++ ARcade Madness/Assets/LukasJ/Scripts/Minigames/FireballFight/FireballRoutine.cs
@@ -6,17 +6,24 @@ public class FireballRoutine : MonoBehaviour
 {
     public float speed = 10f;
     private Rigidbody rb;
+    public GameObject parent;
 
+    private void Awake()
+    {
+        parent = GameObject.Find("Player_Fireball").transform.GetChild(0).gameObject;
+        StartCoroutine(Parent());
+    }
 
     private void Start()
     {
+        
         rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         //rb.velocity = -transform.forward * speed;// * Time.deltaTime;
-        rb.AddForce(-transform.forward * speed * Time.deltaTime, ForceMode.Impulse);
+        rb.AddForce(-parent.transform.forward * speed * Time.deltaTime, ForceMode.Impulse);
         //this.gameObject.transform.Translate(-Vector3.forward * speed * Time.deltaTime);
         //stransform.Translate()
     }
@@ -29,5 +36,12 @@ public class FireballRoutine : MonoBehaviour
         }
 
         Destroy(this.gameObject);
+    }
+
+    IEnumerator Parent ()
+    {
+        transform.parent = parent.transform;
+        yield return new WaitForSeconds(0.1f);
+        transform.parent = null;
     }
 }
