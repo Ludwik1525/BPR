47ac1d4b1b517d15c135e7e11b8b42985b431929
BestProjectMember <38316729+BestProjectMember@users.noreply.github.com>
2020-11-23 13:49:45 +0100

    sync v5

--- ARcade Madness/Assets/LukasJ/Scripts/Sync/FireballSceneSync.cs
+++ ARcade Madness/Assets/LukasJ/Scripts/Sync/FireballSceneSync.cs
@@ -1,98 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Photon.Pun;
 using UnityEngine;
-using Photon.Pun;
 
 public class FireballSceneSync : MonoBehaviour, IPunObservable
 {
-    Transform trn;
-    PhotonView photonView;
+    private PhotonView pv;
+    Vector3 _networkPosition;
+    Quaternion _networkRotation;
+    Rigidbody _rb;
 
-    Vector3 networkedPosition;
-    Quaternion networkedRotation;
-
-    public bool synchronizeVelocity = true;
-    public bool synchronizeAngularVelocity = true;
-    public bool isTeleportEnabled = true;
-    public float teleportIfDistanceGreaterThan = 1.0f;
-
-    private float distance;
-    private float angle;
-
-    private void Awake()
-    {
-        trn = GetComponent<Transform>();
-        photonView = GetComponent<PhotonView>();
-
-        networkedPosition = new Vector3();
-        //networkedRotation = new Quaternion();
-    }
-
-    private void FixedUpdate()
-    {
-        if (!photonView.IsMine)
-        {
-            // update "RemoteME"
-            trn.position = Vector3.MoveTowards(trn.position, networkedPosition, distance * (1.0f / PhotonNetwork.SerializationRate));
-            //rb.rotation = Quaternion.RotateTowards(rb.rotation, networkedRotation, angle * (1.0f / PhotonNetwork.SerializationRate));
-        }
-    }
+    
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.IsWriting)
         {
-            //Then, photonView is mine and I am the one who controls the player
-            //should send postion, velocity etc. data to the other players 
-            stream.SendNext(trn.position);
-            //stream.SendNext(rb.rotation);
-
-            if (synchronizeVelocity)
-            {
-                //stream.SendNext(trn.velocity);
-            }
-
-            //if (synchronizeAngularVelocity)
-            //{
-            //    stream.SendNext(rb.angularVelocity);
-            //}
+            stream.SendNext(_rb.position);
+            stream.SendNext(_rb.rotation);
+            stream.SendNext(_rb.velocity);
         }
         else
         {
-            //Called on my player gameobject that exists in remote player's game
-            networkedPosition = (Vector3)stream.ReceiveNext();
-            //networkedRotation = (Quaternion)stream.ReceiveNext();
+            _networkPosition = (Vector3)stream.ReceiveNext();
+            _networkRotation = (Quaternion)stream.ReceiveNext();
+            _rb.velocity = (Vector3)stream.ReceiveNext();
 
-            if (isTeleportEnabled)
-            {
-                if (Vector3.Distance(trn.position, networkedPosition) > teleportIfDistanceGreaterThan)
-                {
-                    trn.position = networkedPosition;
-                }
-            }
+            float lag = Mathf.Abs((float)(PhotonNetwork.Time - info.timestamp));
+            _networkPosition += (_rb.velocity * lag);
+        }
+    }
 
-            if (synchronizeVelocity || synchronizeAngularVelocity)
-            {
-                float lag = Mathf.Abs((float)(PhotonNetwork.Time - info.SentServerTime));
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        pv = GetComponent<PhotonView>();
+    }
 
-                if (synchronizeVelocity)
-                {
-                    //trn.velocity = (Vector3)stream.ReceiveNext();
-
-                    //networkedPosition += trn.velocity * lag;
-
-                    distance = Vector3.Distance(trn.position, networkedPosition);
-                }
-
-                //if (synchronizeAngularVelocity)
-                //{
-                //    rb.angularVelocity = (Vector3)stream.ReceiveNext();
-
-                //    networkedRotation = Quaternion.Euler(rb.angularVelocity * lag) * networkedRotation;
-
-                //    angle = Quaternion.Angle(rb.rotation, networkedRotation);
-                //}
-            }
+    public void FixedUpdate()
+    {
+        if (!pv.IsMine)
+        {
+            _rb.position = Vector3.MoveTowards(_rb.position, _networkPosition, Time.fixedDeltaTime);
+            _rb.rotation = Quaternion.RotateTowards(_rb.rotation, _networkRotation, Time.fixedDeltaTime * 100.0f);
         }
     }
 }
