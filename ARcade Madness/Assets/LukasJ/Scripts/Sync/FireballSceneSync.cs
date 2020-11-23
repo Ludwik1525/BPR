45f37a2a3bc400b9c406cd44fd2f4a56a05f77e3
Ludwik1526 <mmichal1526@gmail.com>
2020-11-23 14:07:11 +0100

    Sync script v -1

--- ARcade Madness/Assets/LukasJ/Scripts/Sync/FireballSceneSync.cs
+++ ARcade Madness/Assets/LukasJ/Scripts/Sync/FireballSceneSync.cs
@@ -6,7 +6,7 @@ public class FireballSceneSync : MonoBehaviour, IPunObservable
     private PhotonView pv;
     Vector3 _networkPosition;
     Quaternion _networkRotation;
-    Rigidbody _rb;
+    //Rigidbody _rb;
 
     
 
@@ -14,24 +14,24 @@ public class FireballSceneSync : MonoBehaviour, IPunObservable
     {
         if (stream.IsWriting)
         {
-            stream.SendNext(_rb.position);
-            stream.SendNext(_rb.rotation);
-            stream.SendNext(_rb.velocity);
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+            //stream.SendNext(_rb.velocity);
         }
         else
         {
             _networkPosition = (Vector3)stream.ReceiveNext();
             _networkRotation = (Quaternion)stream.ReceiveNext();
-            _rb.velocity = (Vector3)stream.ReceiveNext();
+            //_rb.velocity = (Vector3)stream.ReceiveNext();
 
             float lag = Mathf.Abs((float)(PhotonNetwork.Time - info.timestamp));
-            _networkPosition += (_rb.velocity * lag);
+            //_networkPosition += (_rb.velocity * lag);
         }
     }
 
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();
+        //_rb = GetComponent<Rigidbody>();
         pv = GetComponent<PhotonView>();
     }
 
@@ -39,8 +39,8 @@ public class FireballSceneSync : MonoBehaviour, IPunObservable
     {
         if (!pv.IsMine)
         {
-            _rb.position = Vector3.MoveTowards(_rb.position, _networkPosition, Time.fixedDeltaTime);
-            _rb.rotation = Quaternion.RotateTowards(_rb.rotation, _networkRotation, Time.fixedDeltaTime * 100.0f);
+            transform.position = Vector3.MoveTowards(transform.position, _networkPosition, Time.fixedDeltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, _networkRotation, Time.fixedDeltaTime * 100.0f);
         }
     }
 }
