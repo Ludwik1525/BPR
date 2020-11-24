37591295a55d3197bd5a27426e1bcf0eab61bedb
luakso <38975851+luakso@users.noreply.github.com>
2020-11-24 11:50:53 +0100

    UI update

--- ARcade Madness/Assets/LukasJ/Scripts/MainMenu/RoomController.cs
+++ ARcade Madness/Assets/LukasJ/Scripts/MainMenu/RoomController.cs
@@ -210,9 +210,9 @@ public class RoomController : MonoBehaviourPunCallbacks
         if (!PhotonNetwork.IsMasterClient)
             return;
         //PhotonNetwork.LoadLevel("Spinner_Gameplay");
-        //PhotonNetwork.LoadLevel("Pacman_Gameplay");
+        PhotonNetwork.LoadLevel("Pacman_Gameplay");
 
-        PhotonNetwork.LoadLevel("FireBallFightMiniGame");
+        //PhotonNetwork.LoadLevel("FireBallFightMiniGame");
         //PhotonNetwork.LoadLevel("BoardScene");
     }
 
