ee17c0c647583590cf7bea7b301a46d067481329
luakso <38975851+luakso@users.noreply.github.com>
2020-12-08 13:36:20 +0100

    Update GhostMovement_PC.cs

--- ARcade Madness/Assets/LukasS/Pacman_Game/Scripts/GhostMovement_PC.cs
+++ ARcade Madness/Assets/LukasS/Pacman_Game/Scripts/GhostMovement_PC.cs
@@ -8,10 +8,9 @@ public class GhostMovement_PC : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField]
     private GameObject[] points;
-    [SerializeField]
-    private GameObject playersParent;
+
     bool toMove = true;
-    private int random;
+    private int random = 0;
 
     public float RotationSpeed = 10f;
 
@@ -21,8 +20,6 @@ public class GhostMovement_PC : MonoBehaviour
     Quaternion _lookRotation;
     void Start()
     {
-        playersParent = GameObject.Find("PlayersParent");
-        random = Random.Range(0, playersParent.transform.childCount);
     }
 
     // Update is called once per frame
@@ -39,21 +36,23 @@ public class GhostMovement_PC : MonoBehaviour
         //transform.rotation = Quaternion.RotateTowards(transform.rotation, points[random].transform.rotation , 360 * Time.deltaTime);
 
         print("ranodm " + random);
-        transform.position = Vector3.MoveTowards(transform.position, playersParent.transform.GetChild(random).position, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, points[random].transform.position, speed * Time.deltaTime);
+        //transform.position = Vector3.MoveTowards(transform.position, playersParent.transform.GetChild(random).position, speed * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
     {
+
+
         //if(other.gameObject.CompareTag("PacmanPatrolPoints"))
-        if (other.gameObject.CompareTag("PacmanPlayer"))
+        if (other.gameObject.CompareTag("PacmanPatrolPoints"))
         {
             points = other.gameObject.GetComponent<PossiblePatrolPoints_PC>().points;
-            //random = Random.Range(0, points.Length);
-            random = Random.Range(0, playersParent.transform.childCount);
+            random = Random.Range(0, points.Length);
 
-            _direction = (playersParent.transform.GetChild(random).transform.position - transform.position);
-            //_direction = (points[random].transform.position - transform.position);
+            //_direction = (playersParent.transform.GetChild(random).transform.position - transform.position);
+            _direction = (points[random].transform.position - transform.position);
             _lookRotation = Quaternion.LookRotation(_direction);
-        }    
+        }
     }
 }
