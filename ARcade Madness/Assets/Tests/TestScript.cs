b43decfd7ad281c02d5267e7e84cb125604bf838
BestProjectMember <38316729+BestProjectMember@users.noreply.github.com>
2020-12-16 16:53:23 +0100

    Update TestScript.cs

--- ARcade Madness/Assets/Tests/TestScript.cs
+++ ARcade Madness/Assets/Tests/TestScript.cs
@@ -8,49 +8,43 @@ namespace Tests
 {
     public class TestScript
     {
-        // A Test behaves as an ordinary method
+        //Tests if player spawns in correct position
         [Test]
-        public void TestScriptSimplePasses()
+        public void StartPositionTest()
         {
             GameObject go = new GameObject();
             var gm = go.AddComponent<GameMangerTest>();
-
             gm.SpawnPlayer();
-
             var player = gm.GetPlayer();
             
             Assert.AreEqual(new Vector3(0.2f,0.14f,0.12f), player.transform.position);
         }
 
+        //Tests if player takes damage correctly
         [Test]
         public void DoDamageTest()
         {
             GameObject go = new GameObject();
             var gm = go.AddComponent<GameMangerTest>();
-
             gm.SpawnPlayer();
-
             var player = gm.GetPlayer();
-
             player.GetComponent<BattleTest>().DoDamage(300);
 
-            //Originaly 3600
+            //Player spawns with 3600 health
             Assert.AreEqual(3300, player.GetComponent<SpinTest>().health);
         }
 
+        //Tests if player dies after taking lethal dose of damage
         [Test]
         public void DieTest()
         {
             GameObject go = new GameObject();
             var gm = go.AddComponent<GameMangerTest>();
-
             gm.SpawnPlayer();
-
             var player = gm.GetPlayer();
-
             player.GetComponent<BattleTest>().DoDamage(3600);
 
-            Assert.AreEqual(true, player.GetComponent<BattleTest>().isDead );
+            Assert.AreEqual(true, player.GetComponent<BattleTest>().isDead);
         }
 
 
